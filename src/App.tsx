83ca064975b2ccc1388686bdceef81579bb9ae90
lonelil <51315646+lonelil@users.noreply.github.com>
2023-02-26 14:50:42 +0800

    made it look nicer

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Redirect from "./Redirect";
 export default function App() {
   return (
-    <div className="m-auto text-center">
+    <div className="m-auto text-center mt-12">
       <img
-        src="https://cdn.discordapp.com/attachments/927030276394217482/993629387012448306/logowhite.svg"
+        src="https://cdn.discordapp.com/attachments/927030276394217482/993629293714350110/logowhite.svg"
         alt="Logo"
-        className="w-96 mx-auto"
+        className="w-28 mb-4 mx-auto"
       />
       <Routes>
         <Route path="*" element={<Redirect />} />
       </Routes>
-      <a href="https://prank.gg" className="text-xs block text-zinc-400">
+      <a href="https://prank.gg" className="text-xs block text-zinc-400 mt-1">
         Go back to Prank.GG.
       </a>
     </div>
